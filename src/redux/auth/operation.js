7009e22d0823c3f43e20c7516998334b37f6e1de
Nazar Marchenko <109473164+MarchenkoNaz@users.noreply.github.com>
2023-08-09 14:46:16 +0300

    fixing

--- src/redux/auth/operation.js
+++ src/redux/auth/operation.js
@@ -7,6 +7,7 @@ const setAuthHeader = token => {
 	axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
+
 const clearAuthHeader = () => {
 	axios.defaults.headers.common.Authorization = '';
 };
